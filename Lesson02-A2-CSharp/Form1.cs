138ac75c5461a37a95bb8af63073643e274005dc
slyw0lf <g.pimpinella@outlook.it>
2022-10-11 17:33:40 +0200

    Changed Timer

--- Lesson02-A2-CSharp/Form1.cs
+++ Lesson02-A2-CSharp/Form1.cs
@@ -13,9 +13,8 @@ namespace Lesson02_A2_CSharp
 {
     public partial class Form1 : Form
     {
-        private System.Timers.Timer myTimer;
+        private int counter;
         private Random rand = new Random();
-
 
         public Form1()
         {
@@ -27,12 +26,6 @@ namespace Lesson02_A2_CSharp
            
         }
 
-        private void onTimerElapsed(Object source, ElapsedEventArgs e)
-        {
-            setRandBackgroundColor();
-
-        }
-
         private void setRandBackgroundColor()
         {
             Color randColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
@@ -41,15 +34,49 @@ namespace Lesson02_A2_CSharp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            myTimer = new System.Timers.Timer(1500);
-            myTimer.Elapsed += onTimerElapsed;
-            myTimer.Enabled = true;
-            myTimer.AutoReset = true;
+            /* myTimer = new System.Timers.Timer(1500);
+             myTimer.Elapsed += onTimerElapsed;
+             myTimer.Enabled = true;
+             myTimer.AutoReset = true;*/
+            counter = 2 + rand.Next(8);
+            timer1.Interval = 1000;
 
             button1.Visible = false;
 
+            label1.Visible = true;
+            label2.Text = counter.ToString();
+            label2.Visible = true;
+            button2.Visible = true;
+
             setRandBackgroundColor();
 
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            counter--;
+
+            if(counter == 0)
+            {
+                timer1.Stop();
+                setRandBackgroundColor();
+
+                counter = 2 + rand.Next(8);
+                timer1.Start();
+            }
+
+            label2.Text = counter.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            label1.Visible = false;
+            label2.Visible = false;
+            button2.Visible = false;
+            button1.Visible = true;
         }
     }
 }
